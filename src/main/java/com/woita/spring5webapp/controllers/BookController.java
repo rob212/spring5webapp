522c55d44a718316f671dc71799cc964e9b91be9
Rob McBryde <robertmcbryde83@gmail.com>
2019-07-25 17:06:16 +0100

    Addition of a book controller to retrieve a list of all books and call yet to be created view

---
+++ src/main/java/com/woita/spring5webapp/controllers/BookController.java
@@ -0,0 +1,26 @@
+package com.woita.spring5webapp.controllers;
+
+import com.woita.spring5webapp.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author mcbrydr on 25/07/19
+ */
+@Controller
+public class BookController {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @RequestMapping("/books")
+    public String getBooks(Model model){
+        // Spring MVC will pass in an instance of the model. We then add an attribute to the model calls books which is
+        // a list of all the books, via the book repository which underneath the covers is using JPA and Hibernate from the DB
+        model.addAttribute("books", bookRepository.findAll());
+        // Thymeleaf view name
+        return "books";
+    }
+}
